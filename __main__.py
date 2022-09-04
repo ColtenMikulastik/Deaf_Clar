ac32e511a4261ad32101aade8fc8ed4e073c5d7e
Colten <mikulastikc@gmail.com>
2022-09-03 20:53:34 -0500

    added comments

--- __main__.py
+++ __main__.py
@@ -2,12 +2,17 @@
 from socket import *
 
 def main():
+    # ask for user input
     target = input("Enter the host to be scanned: ")
     print("starting scan on host: " + target)
 
+    # loop through ports
     for i in range(1, 500):
+        # create a socket object
         s = socket(AF_INET, SOCK_STREAM)
+        # attempt to connect to target at i port
         conn = s.connect_ex((target, i))
+        # print port number if found
         if conn == 0:
             print("Port " + str(i) + ": Open")
         s.close()
